f51b9c9f4ef99b798500f6b9281c17ed652f36a6
pupsikus <voronov.a.g@gmail.com>
2017-09-17 14:47:22 +0300

    fixed urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,22 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^viewer-count/'
-        r'(?start=(?P<start_timestamp>\w+))'
-        r'(?end=(?P<end_timestamp>\w+))'
-        r'(?device=(?P<device_id>\d+))'
-        r'(?content=(?P<content_id>\d+))$', views.viewer_count,
+    url(r'^viewer-count/$', views.viewer_count,
         name='viewer_count'),
-    url(r'^avg-age/'
-        r'(?start=(?P<start_timestamp>\w+))'
-        r'(?end=(?P<end_timestamp>\w+))'
-        r'(?device=(?P<device_id>\d+))'
-        r'(?content=(?P<content_id>\d+))$', views.avg_age,
+    url(r'^avg-age/$', views.avg_age,
         name='avg_age'),
-    url(r'^gender-dist/'
-        r'(?start=(?P<start_timestamp>\w+))'
-        r'(?end=(?P<end_timestamp>\w+))'
-        r'(?device=(?P<device_id>\d+))'
-        r'(?content=(?P<content_id>\d+))$', views.gender_dist,
+    url(r'^gender-dist/$', views.gender_dist,
         name='gender_dist')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,20 +4,26 @@ from django.http.response import JsonResponse
 from .models import *
 
 
-def viewer_count(request, start_timestamp, end_timestamp, device_id,
-                 content_id):
-    content = get_object_or_404(Content, pk=content_id)
-    # return render(request, 'dashboard/viewer_count.html', {'content': content})
+def viewer_count(request):
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    device_id = request.GET.get('device')
+    content_id = request.GET.get('content')
+    # content = get_object_or_404(Content, pk=content_id)
+    return JsonResponse({'content': content_id})
+
+
+def avg_age(request):
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    device_id = request.GET.get('device')
+    content_id = request.GET.get('content')
     return JsonResponse({'content': content})
 
 
-def avg_age(request, start_timestamp, end_timestamp, device_id, content_id):
-    content = get_object_or_404(Content, pk=content_id)
-    # return render(request, 'dashboard/viewer_count.html', {'content': content})
-    return JsonResponse({'content': content})
-
-def gender_dist(request, start_timestamp, end_timestamp, device_id,
-                content_id):
-    content = get_object_or_404(Content, pk=content_id)
-    # return render(request, 'dashboard/viewer_count.html', {'content': content})
+def gender_dist(request):
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    device_id = request.GET.get('device')
+    content_id = request.GET.get('content')
     return JsonResponse({'content': content})
